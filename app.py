254a4c3f7680ceb0746dfe5b1958a0dd14f6b155
pullemlaabhinav-dot <pullemlaabhinav@gmail.com>
2026-04-08 22:46:01 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -221,5 +221,8 @@ def timetable():
     return render_template('timetable.html',data=data)
 
 
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
